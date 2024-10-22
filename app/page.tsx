cb5ad872503f412a64690a72bc6dd381cc8f65bb
KKaged <dhigaredaleon707@gmail.com>
2024-10-22 11:42:11 +0900

    edited blob text to fit properly

--- app/page.tsx
+++ app/page.tsx
@@ -16,45 +16,48 @@ const companyBody = [
   "広告代理業務: 効果的なプロモーションと宣伝活動の支援。",
 ];
 
-
 export default function Home() {
   const [isClicked, setIsClicked] = useState(false);
-  
+
   return (
     <main className="flex flex-col justify-between h-dvh overflow-x-hidden">
       <Header />
       <main className="h-[1000px] w-full flex justify-center items-center bg-[#f5f5dc]">
-      <div className="h-full w-full flex items-center justify-center">
-        <div
-          onClick={() => setIsClicked(!isClicked)}
-          role="button"
-          className="bg-contain bg-no-repeat bg-center bg-[url('/orangeBlob.webp')] h-dvh w-fit md:w-5/6 md:h-5/6 flex flex-col items-center justify-around"
-        >
-          {!isClicked ? (
-            <Image src={blob} alt="pepe" className="w-1/4" />
-          ) : (
-            <div className="h-3/4 w-3/4 text-center flex flex-col justify-evenly items-center text-white">
-              <h2 className="text-xl p-2">会社概要</h2>
-              <p className="font-bold w-2/3">
-                弊社は現在、英語学習サービスを提供しています。
-                <br />
-                将来的には、以下の分野での展開を目指しています。
-              </p>
-              <ul className="list-disc list-inside font-bold text-left">
-                {companyBody.map((areas) => (
-                  <li key={areas.length}>{areas}</li>
-                ))}
-              </ul>
-              <p className="font-bold w-1/2">
-                私たちは、お客様のニーズに応じたサービスを提供し、
-                <br />
-                共に成長するパートナーとして活動してまいります。
-              </p>
-            </div>
-          )}
+        <div className="p-10 h-full w-full flex items-center justify-center">
+          <div
+            onClick={() => setIsClicked(!isClicked)}
+            role="button"
+            className="bg-contain bg-no-repeat bg-center bg-[url('/orangeBlob.webp')] h-[70vh] w-[70vw] md:h-[75vh] md:w-[75vw] flex flex-col items-center justify-around"
+          >
+            {!isClicked ? (
+              <Image src={blob} alt="pepe" className="w-1/3" />
+            ) : (
+              <div className="h-3/4 w-3/4 text-center flex flex-col justify-evenly items-center text-white">
+                <h2 className="text-lg font-bold p-2">会社概要</h2>
+                <p className="font-medium  text-s w-2/3">
+                  弊社は現在、英語学習サービスを提供しています。
+                  <br />
+                  将来的には、以下の分野での展開を目指しています。
+                </p>
+                <ul className="list-disc list-inside text-s font-medium text-left w-2/3 flex flex-col items-start">
+                  {" "}
+                  {/* Left-align list */}
+                  {companyBody.map((areas, index) => (
+                    <li key={index} className="w-full">
+                      {areas}
+                    </li>
+                  ))}
+                </ul>
+                <p className="font-medium text-s w-1/2">
+                  私たちは、お客様のニーズに応じたサービスを提供し、
+                  <br />
+                  共に成長するパートナーとして活動してまいります。
+                </p>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
       <Footer />
     </main>
   );
